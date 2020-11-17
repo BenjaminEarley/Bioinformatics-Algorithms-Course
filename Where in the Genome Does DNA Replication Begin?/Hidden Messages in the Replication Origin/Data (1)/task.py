14a8202cbea0d8dbcecb0a9946f594003962fe01
Benjamin Earley <thebigben92@gmail.com>
2020-11-17 17:04:56 -0500

    Task 1.2 Complete

--- Where in the Genome Does DNA Replication Begin?/Hidden Messages in the Replication Origin/Data (1)/task.py
+++ Where in the Genome Does DNA Replication Begin?/Hidden Messages in the Replication Origin/Data (1)/task.py
@@ -1,2 +1,32 @@
 #  This is a data task. You can use this editor as a playground
 
+with open("../../../data/2.txt") as fp:
+    lines = fp.read().splitlines()
+
+text = lines[0]
+k = int(lines[1])
+
+
+def count_pattern(data, pattern):
+    count = 0
+    patternLength = len(pattern)
+    for i in range(len(data) - patternLength + 1):
+        if data[i:i + patternLength] == pattern:
+            count += 1
+    return count
+
+
+def frequent_words(data, pattern_length):
+    frequentPatterns = set()
+    count = []
+    for i in range(len(data) - pattern_length + 1):
+        pattern = data[i:i + pattern_length]
+        count.append(count_pattern(data, pattern))
+    maxCount = max(count)
+    for i in range(len(data) - pattern_length + 1):
+        if count[i] == maxCount:
+            frequentPatterns.add(data[i:i + pattern_length])
+    return ' '.join(map(str, [*frequentPatterns]))
+
+
+print(frequent_words(text, k))
--- Where in the Genome Does DNA Replication Begin?/Hidden Messages in the Replication Origin/Theory (7)/task.py
+++ Where in the Genome Does DNA Replication Begin?/Hidden Messages in the Replication Origin/Theory (7)/task.py
@@ -1,2 +1,32 @@
 #  You can experiment here, it won’t be checked
 
+text = "ACTGACTCCCACCCC"
+k = 3
+
+
+def count_pattern(data, pattern):
+    count = 0
+    patternLength = len(pattern)
+    for i in range(len(data) - patternLength + 1):
+        if data[i:i + patternLength] == pattern:
+            count += 1
+    return count
+
+
+def frequent_words(data, k):
+    frequentPatterns = set()
+    count = []
+    for i in range(len(data) - k + 1):
+        pattern = data[i:i + k]
+        count.append(count_pattern(data, pattern))
+    maxCount = max(count)
+    for i in range(len(data) - k + 1):
+        if count[i] == maxCount:
+            frequentPatterns.add(data[i:i + k])
+    return ' '.join(map(str, [*frequentPatterns]))
+
+
+print(frequent_words(text, k))
+
+
+
